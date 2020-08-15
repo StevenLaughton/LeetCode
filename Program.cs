12d756a46caf6d05e894784c95895f1ca82368d6
steven.laughton@gmail.com <steven.laughton@gmail.com>
2020-08-15 18:00:55 +0100

    Correct value

--- Program.cs
+++ Program.cs
@@ -8,13 +8,30 @@ namespace Leetcode
     {
         static void Main(string[] args)
         {
-            var res = Reverse(12);
+            var res = Reverse(1534236469);
+            Console.WriteLine(res);
         }
 
         public static int Reverse(int x)
         {
             var strung = x.ToString();
-            return 1;
+            var sign = "";
+
+            if(strung[0] == '-')
+            {
+                sign = "-";
+                strung = strung.Substring(1);
+            }
+
+            var reversed = "";
+            for (int i = 0; i < strung.Length; i++)
+            {
+                reversed += strung[strung.Length - (1 + i)];
+            }
+
+            var parsed = int.TryParse($"{sign}{reversed}", out var res);
+
+            return parsed ? res : 0;
         }
     }
 
