75281f67e1085832fa1aafcbed38ba6af68a7ae1
steven.laughton@gmail.com <steven.laughton@gmail.com>
2020-08-16 13:19:33 +0100

    Completed

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Leetcode
+﻿using System;
+using System.Linq;
+
+namespace Leetcode
 {
     public class Program
     {
         public static void Main()
         {
+            var input = new string[] { "flower", "flow", "flight" };
+            var res = LongestCommonPrefix(input);
+        }
 
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var prefix = string.Empty;
+            if (strs.Count() > 0)
+            {
+                var len = strs.Min(x => x.Length);
+                for (var i = 0; i < len; i++)
+                {
+                    var values = strs.Select(s => s[i]).Distinct();
+                    if (values.Count() == 1)
+                    {
+                        prefix += values.First();
+                    }
+                    else
+                    {
+                        return prefix;
+                    }
+                }
+            };
+            return prefix;
         }
     }
 }
