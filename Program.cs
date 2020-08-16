3b6e0837a8d1b70682275283087a304121993ab7
steven.laughton@gmail.com <steven.laughton@gmail.com>
2020-08-16 12:20:42 +0100

    completed

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Leetcode
+﻿namespace Leetcode
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var res = IsPalindrome(1441);
 
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            var asString = x.ToString();
+
+            for (var i = 0; i < (asString.Length / 2); i++)
+            {
+                if (!Equals(asString[i], asString[^(i + 1)]))
+                {
+                    return false;
+                };
+            }
+
+            return true;
         }
     }
 
