faeb0d01ebc6e5b03f40ee24a67f869532927ce3
steven.laughton@gmail.com <steven.laughton@gmail.com>
2020-10-11 16:34:52 +0100

    Cleaned main

--- Main.ts
+++ Main.ts
@@ -1,20 +1,5 @@
 class Startup {
-  public static main(): void {
-    let result = removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
-
-    console.log(result);
-  }
-}
-
-function removeDuplicates(nums: number[]): number {
-  for (let index = 0; index < nums.length; ) {
-    if (index !== 0 && nums[index] === nums[index - 1]) {
-      nums.splice(index, 1);
-    } else {
-      index++;
-    }
-  }
-  return nums.length;
+  public static main(): void {}
 }
 
 Startup.main();
